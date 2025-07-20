a48d44e8900b6722c6468d552d52459df4269141
hestudy <1467602180@qq.com>
2025-07-20 13:21:13 +0800

    ✨ 引入侧边栏布局并集成 Radix UI 组件与调试插件

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+const { codeInspectorPlugin } = require('code-inspector-plugin');
 
 export default defineConfig({
   plugins: [pluginReact()],
   resolve: {
     alias: {
       '@': './src',
+    },
+  },
+  tools: {
+    rspack: {
+      plugins: [
+        codeInspectorPlugin({
+          bundler: 'rspack',
+        }),
+      ],
     },
   },
 });
---
+++ src/layouts/sidebarLayout.tsx
@@ -0,0 +1,49 @@
+import { AppSidebar } from '@/components/app-sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { memo } from 'react';
+import { Outlet } from 'react-router';
+
+const SidebarLayout = memo(() => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="size-full bg-background p-2">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+});
+
+export default SidebarLayout;
